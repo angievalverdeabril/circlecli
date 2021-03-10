17f8488777c1fd7b04aaf9b28c812543f2d9b004
Angie Paola Valverde Abril <abril.angie13@gmail.com>
2021-03-10 15:22:08 -0500

    sync between github and circlecli

--- browser.test.js
+++ browser.test.js
@@ -21,7 +21,7 @@ test('get textbox value', async () => {
 test('get results values', async () => {   
   const value =  await fnc.getResults(driver);
   console.log("los resultados " +  value + ";");
-  expect(value).toEqual(value + "sadsa");
+  expect(value).toEqual(value + "444");
   driver.quit();
 });
 
