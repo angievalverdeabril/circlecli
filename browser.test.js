83d98ebf07d57b0ceae506b12b6d4e3249fb9a69
Angie Paola Valverde Abril <abril.angie13@gmail.com>
2021-03-12 05:11:32 -0500

    fixed the code2

--- browser.test.js
+++ browser.test.js
@@ -19,7 +19,8 @@ test('get textbox value', async () => {
 });
 
 test('get results values', async () => {   
-  //added a line comment
+  //added a line comment  
+  // new chnge
   const value =  await fnc.getResults(driver);
   console.log("los resultados " +  value + ";");
   expect(value).toEqual(value);
